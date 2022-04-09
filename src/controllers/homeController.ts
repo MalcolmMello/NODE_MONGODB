6253834706d1ac9947788bc17b84c31c0cec17f8
MalcolmMello <malcolmmellolima@gmail.com>
2022-04-09 15:10:13 -0300

    atualizar um usuario

--- src/controllers/homeController.ts
+++ src/controllers/homeController.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from 'express';
-
-import { Product } from '../models/Product';
 import User from '../models/User';
 
 export const home = async (req: Request, res: Response) => {
+    let paulo = await User.findOne({ email: "paulo@hotmail.com.br" })
+    if(paulo) {
+        paulo.name.lastName = "TroqueiOSobrenome"
+        paulo.age = 83
+    }
+    await paulo?.save()
+    
     let users = await User.find({}).sort({ "name.firstName": 1 });
 
     res.render('pages/home', {
